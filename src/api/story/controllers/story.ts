95a9022292b1ca46fe0919a0b5969996eba0024c
vladislavpuzyrev <vpuzyrev@unistory.app>
2025-11-21 17:24:49 +0300

    add: custtom controller for views

--- src/api/story/controllers/story.ts
+++ src/api/story/controllers/story.ts
@@ -4,4 +4,67 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::story.story');
+export default factories.createCoreController('api::story.story', ({ strapi }) => ({
+  async find(ctx) {
+    const query = ctx.query ?? {}
+    const { phoneNumber, ...restQuery } = query
+
+    ctx.query = restQuery
+    const response = await super.find(ctx)
+    const stories = response.data || []
+
+    if (!stories.length) {
+      return response
+    }
+
+    if (!phoneNumber) {
+      response.data = stories.map((story) => ({
+        ...story,
+        attributes: {
+          ...story.attributes,
+          isViewed: false,
+        },
+      }))
+      return response
+    }
+
+    const storyIds = stories.map((story) => story.id)
+    const storyViews = (await strapi.entityService.findMany(
+      'api::story-view.story-view' as any,
+      {
+        fields: ['id'],
+        filters: {
+          phoneNumber,
+          story: {
+            id: {
+              $in: storyIds,
+            },
+          },
+        },
+        populate: {
+          story: {
+            fields: ['id'],
+          },
+        },
+        limit: storyIds.length,
+      }
+    )) as Array<{ story?: { id?: string | number | null } | null }>
+
+    const viewedStoryIds = new Set(
+      storyViews
+        .map((view) => view?.story?.id)
+        .filter((id): id is string | number => typeof id === 'number' || typeof id === 'string')
+        .map((id) => String(id))
+    )
+
+    response.data = stories.map((story) => ({
+      ...story,
+      attributes: {
+        ...story.attributes,
+        isViewed: viewedStoryIds.has(String(story.id)),
+      },
+    }))
+
+    return response
+  },
+}))
